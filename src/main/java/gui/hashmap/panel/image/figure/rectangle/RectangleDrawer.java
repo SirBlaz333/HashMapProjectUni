5900deb08ac3632b27c5f75c19ce3797b44d1882
Kateryna Novikova <0802novikova@gmail.com>
2022-10-12 21:18:58 +0300

    changed draw(), added setColor() in RectangleDrawer

--- src/main/java/gui/hashmap/panel/image/figure/rectangle/RectangleDrawer.java
+++ src/main/java/gui/hashmap/panel/image/figure/rectangle/RectangleDrawer.java
@@ -1,25 +1,24 @@
 package gui.hashmap.panel.image.figure.rectangle;
 
+import gui.hashmap.panel.image.Colorable;
+import gui.hashmap.panel.image.figure.Figure;
+
 import java.awt.*;
 
-public class RectangleDrawer {
-    private int side = 100;
-    private Color color = Color.blue;
-    private boolean filling = false;
+public class RectangleDrawer implements Figure, Colorable {
+    private final int side;
+    private Color color;
 
-    public RectangleDrawer(){}
-
-    public RectangleDrawer(int side, Color color, boolean filling) {
+    public RectangleDrawer(int side) {
         this.side = side;
-        this.color = color;
-        this.filling = filling;
     }
 
+    @Override
     public void draw(Graphics graphics, int x, int y){
-        graphics.setColor(color);
         graphics.drawRect(x, y, side, side);
-        if(filling){
-            graphics.fillRect(x, y, side, side);
-        }
+    }
+    @Override
+    public void setColor(Color color) {
+        this.color = color;
     }
 }
