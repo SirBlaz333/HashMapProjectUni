c69d60716d1c89850f5c0adce840edd4c884b2ea
sadmoony <moony.akk@gmail.com>
2022-10-12 20:23:33 +0300

    new

---
+++ src/main/java/gui/hashmap/panel/MapList.java
@@ -0,0 +1,20 @@
+package gui.hashmap.panel;
+
+import java.awt.*;
+
+public class MapList {
+    private int radius;
+    private int x, y;
+
+    public MapList(){}
+
+    public MapList(int radius, int x, int y) {
+        this.radius = radius;
+        this.y = y;
+        this.x = x;
+    }
+
+    public void draw(Graphics graphics, int x, int y){
+        graphics.drawOval(x, y, radius, radius);
+    }
+}
