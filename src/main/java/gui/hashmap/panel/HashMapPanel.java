2135fe02357ed44d44b76ae331b2d7a6073c6e5b
SirBlaz333 <89386578+SirBlaz333@users.noreply.github.com>
2022-10-12 20:16:55 +0300

    Update HashMapPanel.java

--- src/main/java/gui/hashmap/panel/HashMapPanel.java
+++ src/main/java/gui/hashmap/panel/HashMapPanel.java
@@ -19,18 +19,5 @@ public class HashMapPanel extends GUIPanel {
 
     public void setImage(Image image){
         imageIcon.setImage(image);
-        Graphics graphics = image.getGraphics();
-
-        Arrow arrow1 = new Arrow(40, 20, Color.BLUE);
-        arrow1.draw(graphics, 100, 150);
-        arrow1.fill(graphics, 100, 150);
-
-        Arrow arrow2 = new Arrow(60, 30, Color.RED);
-        arrow2.draw(graphics, 100, 200);
-        arrow2.fill(graphics, 100, 200);
-
-        Arrow arrow3 = new Arrow(80, 40, Color.GREEN);
-        arrow3.draw(graphics, 100, 250);
-        arrow3.fill(graphics, 100, 250);
     }
 }
