2292a779a474a7816351d9042ca0c7de8338fff2
vladyslavbezruk <bezrukvladislav@gmail.com>
2022-10-12 21:25:32 +0300

    Changed class ArrowDrawer, aded Figure and Colorable implementation.

--- src/main/java/gui/hashmap/panel/image/figure/arrow/ArrowDrawer.java
+++ src/main/java/gui/hashmap/panel/image/figure/arrow/ArrowDrawer.java
@@ -1,18 +1,21 @@
 package gui.hashmap.panel.image.figure.arrow;
 
+import gui.hashmap.panel.image.Colorable;
+import gui.hashmap.panel.image.figure.Figure;
+
 import java.awt.*;
 
-public class ArrowDrawer {
-    private int width;
-    private int height;
+public class ArrowDrawer implements Figure, Colorable {
+    private final int width;
+    private final int height;
     private Color color;
 
-    public ArrowDrawer(int width, int height, Color color) {
+    public ArrowDrawer(int width, int height) {
         this.width = width;
         this.height = height;
-        this.color = color;
     }
 
+    @Override
     public void draw(Graphics graphics, int x, int y) {
         Polygon polygon = new Polygon(
                 new int[]{
@@ -39,57 +42,11 @@ public class ArrowDrawer {
 
         graphics.setColor(color);
         graphics.drawPolygon(polygon);
+        graphics.fillPolygon(polygon);
     }
 
-    public int getHeight() {
-        return height;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public Color getColor() {
-        return color;
-    }
-
+    @Override
     public void setColor(Color color) {
         this.color = color;
-    }
-
-    public void fill(Graphics graphics, int x, int y) {
-        Polygon polygon = new Polygon(
-                new int[]{
-                        x + (int) (0.75 * width),
-                        x + width,
-                        x + width,
-                        x + (int) (0.75 * width),
-                        x + (int) (0.75 * width),
-                        x,
-                        x,
-                        x + (int) (0.75 * width)
-                },
-                new int[]{
-                        y,
-                        y + (int) (0.5 * height),
-                        y + (int) (0.5 * height),
-                        y + height,
-                        y + (int) (0.7 * height),
-                        y + (int) (0.7 * height),
-                        y + (int) (0.3 * height),
-                        y + (int) (0.3 * height)
-                },
-                8);
-
-        graphics.setColor(color);
-        graphics.fillPolygon(polygon);
     }
 }
