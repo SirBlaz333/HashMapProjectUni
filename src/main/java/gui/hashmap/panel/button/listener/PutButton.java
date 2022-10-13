515458aed9324f982acb29db95b556fa816a10ea
Arsenii Serebriakov <Arsenii_Serebriakov@epam.com>
2022-10-13 17:08:59 +0300

    Add PutButton

---
+++ src/main/java/gui/hashmap/panel/button/listener/PutButton.java
@@ -0,0 +1,16 @@
+package gui.hashmap.panel.button.listener;
+
+import gui.hashmap.panel.HashMapPanel;
+import gui.hashmap.panel.image.ImageProducer;
+
+import javax.swing.*;
+import java.util.Map;
+
+public class PutButton extends JButton {
+    private static final String TEXT = "Put";
+
+    public PutButton(JTextField keyTextField, JTextField valueTextField, Map<Integer, Integer> map, HashMapPanel hashMapPanel, ImageProducer imageProducer){
+        addActionListener(new PutListener(keyTextField, valueTextField, map, hashMapPanel, imageProducer));
+        setText(TEXT);
+    }
+}
