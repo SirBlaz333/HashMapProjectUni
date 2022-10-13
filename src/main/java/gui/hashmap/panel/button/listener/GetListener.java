e14f259c5f3fc0521dd9313190420defbf291c84
vladyslavbezruk <bezrukvladislav@gmail.com>
2022-10-13 16:49:52 +0300

    Aded GetListener class.

---
+++ src/main/java/gui/hashmap/panel/button/listener/GetListener.java
@@ -0,0 +1,28 @@
+package gui.hashmap.panel.button.listener;
+
+import gui.hashmap.panel.HashMapPanel;
+import gui.hashmap.panel.image.ImageProducer;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Map;
+
+public class GetListener implements ActionListener {
+    private final JTextField keyTextField;
+    private final JTextField valueTextField;
+    private final Map<Integer, Integer> map;
+
+    public GetListener(JTextField keyTextField, JTextField valueTextField1, Map<Integer, Integer> map) {
+        this.keyTextField = keyTextField;
+        this.valueTextField = valueTextField1;
+        this.map = map;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Integer key = Integer.getInteger(keyTextField.getText());
+        Integer value = map.get(key);
+        valueTextField.setText(value.toString());
+    }
+}
