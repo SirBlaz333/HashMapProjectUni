5b82f85af4941fb0cb61806fa705838ea1c74f31
Arsenii Serebriakov <Arsenii_Serebriakov@epam.com>
2022-10-16 20:21:47 +0300

    Remove HashMapPanel and ImageProducer dependency from PutButton and PutListener

--- src/main/java/gui/hashmap/panel/button/listener/PutListener.java
+++ src/main/java/gui/hashmap/panel/button/listener/PutListener.java
@@ -1,8 +1,5 @@
 package gui.hashmap.panel.button.listener;
 
-import gui.hashmap.panel.HashMapPanel;
-import gui.hashmap.panel.image.ImageProducer;
-
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,15 +9,11 @@ public class PutListener implements ActionListener {
     private final JTextField keyTextField;
     private final JTextField valueTextField;
     private final Map<Integer, Integer> map;
-    private final HashMapPanel hashMapPanel;
-    private final ImageProducer imageProducer;
 
-    public PutListener(JTextField keyTextField, JTextField valueTextField, Map<Integer, Integer> map, HashMapPanel hashMapPanel, ImageProducer imageProducer) {
+    public PutListener(JTextField keyTextField, JTextField valueTextField, Map<Integer, Integer> map) {
         this.keyTextField = keyTextField;
         this.valueTextField = valueTextField;
         this.map = map;
-        this.hashMapPanel = hashMapPanel;
-        this.imageProducer = imageProducer;
     }
 
     @Override
@@ -28,6 +21,5 @@ public class PutListener implements ActionListener {
         Integer key = Integer.parseInt(keyTextField.getText());
         Integer value = Integer.parseInt(valueTextField.getText());
         map.put(key, value);
-        hashMapPanel.setImage(imageProducer.produce(map));
     }
 }
