86c4d149dc36a30833c23c0af706373ebb1b1a6f
Arsenii Serebriakov <Arsenii_Serebriakov@epam.com>
2022-10-16 21:06:17 +0300

    Add DrawImageListener, which draws image in HashMapPanel. Remove button classes. And other minor fixes

--- src/main/java/gui/hashmap/panel/HashMapPanel.java
+++ src/main/java/gui/hashmap/panel/HashMapPanel.java
@@ -1,23 +1,28 @@
 package gui.hashmap.panel;
 
+import gui.hashmap.panel.image.ImageProducer;
+
 import javax.swing.*;
 import java.awt.*;
+import java.util.Map;
 
 public class HashMapPanel extends GUIPanel {
-    private ImageIcon imageIcon;
+    private JLabel imageLabel;
+    private ImageProducer imageProducer;
     public HashMapPanel(LayoutManager layoutManager) {
         super(layoutManager);
     }
 
     @Override
     protected void init() {
-        imageIcon = new ImageIcon();
-        JLabel label = new JLabel(imageIcon);
-        label.setHorizontalAlignment(SwingConstants.CENTER);
-        this.add(label, BorderLayout.CENTER);
+        imageLabel = new JLabel();
+        imageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        this.add(imageLabel, BorderLayout.CENTER);
+        imageProducer = new ImageProducer(100);
     }
 
-    public void setImage(Image image){
-        imageIcon.setImage(image);
+    public void drawImage(Map<Integer, Integer> map){
+        Image image = imageProducer.produce(map);
+        imageLabel.setIcon(new ImageIcon(image));
     }
 }
