7b8498cb73f55b4ce8a8ef185c91930d1b52b9a9
Arsenii Serebriakov <Arsenii_Serebriakov@epam.com>
2022-10-22 21:30:05 +0300

    Remove IntegerFormatter.java

--- src/main/java/gui/hashmap/panel/button/listener/PutListener.java
+++ src/main/java/gui/hashmap/panel/button/listener/PutListener.java
@@ -18,12 +18,8 @@ public class PutListener implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        try{
-            Integer key = Integer.parseInt(keyTextField.getText());
-            Integer value = Integer.parseInt(valueTextField.getText());
-            map.put(key, value);
-        } catch (NumberFormatException exception){
-            System.err.println("Cannot read integer! " + exception.getMessage());
-        }
+        Integer key = Integer.parseInt(keyTextField.getText());
+        Integer value = Integer.parseInt(valueTextField.getText());
+        map.put(key, value);
     }
 }
