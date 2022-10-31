700214a53eea65aec952f5832604b4f213ce369e
robert-poulsen <dagovecvalerij@gmail.com>
2022-10-31 20:44:17 +0300

    add ErrorMessageRemove and empty HashMapPanel

--- src/main/java/gui/hashmap/panel/listener/button/ClearListener.java
+++ src/main/java/gui/hashmap/panel/listener/button/ClearListener.java
@@ -4,6 +4,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Map;
 
+/**
+ * Клас реалізує подію натиснення кнопки Clear: видаляє всі елементи з HashMap, після чого HashMap є пустою
+ */
+
 public class ClearListener implements ActionListener {
     private final Map<Integer, Integer> map;
 
