692f454c666c9385a0cd77dea80e86019c677f31
robert-poulsen <dagovecvalerij@gmail.com>
2022-12-21 19:25:37 +0200

    add ErrorMessageRemove and empty HashMapPanel

--- src/main/java/gui/hashmap/panel/image/text/TextDrawerImpl.java
+++ src/main/java/gui/hashmap/panel/image/text/TextDrawerImpl.java
@@ -2,11 +2,13 @@ package gui.hashmap.panel.image.text;
 
 import java.awt.*;
 import java.awt.font.TextLayout;
+import java.util.Map;
 
 /**
  * Клас для тексту
  */
 public class TextDrawerImpl implements TextDrawer {
+    private static final int NUMBER_LENGTH = 5;
     private final int fontSize;
     private final int height;
     private final int width;
@@ -14,10 +16,11 @@ public class TextDrawerImpl implements TextDrawer {
 
     /**
      * Конструктор
+     *
      * @param fontSize розмір шрифта
-     * @param height висота об'єкта в якому буде намальовани текст
-     * @param width ширина об'єкта в якому буде намальовани текст
-     * @param color колір
+     * @param height   висота об'єкта в якому буде намальовани текст
+     * @param width    ширина об'єкта в якому буде намальовани текст
+     * @param color    колір
      */
     public TextDrawerImpl(int fontSize, int height, int width, Color color) {
         this.fontSize = fontSize;
@@ -27,26 +30,36 @@ public class TextDrawerImpl implements TextDrawer {
     }
 
     @Override
-    public void draw(Graphics graphics, int x, int y, int number) {
-        String text = Integer.toString(number);
-        if (text.length() > 7) {
-            text = text.substring(0, 7);
-            text = text.concat("...");
-        }
+    public void draw(Graphics graphics, int x, int y, Map.Entry<Integer, Integer> entry) {
+        String textKey = reduceString(Integer.toString(entry.getKey()));
+        String textValue = reduceString(Integer.toString(entry.getValue()));
+
         graphics.setColor(this.color);
         Font font = new Font("Arial", Font.BOLD, fontSize);
         graphics.setFont(font);
-        drawTextInCenter(text, graphics, x, y);
+        drawTextInCenter(textKey, textValue, graphics, x, y);
+    }
+
+    private String reduceString(String string) {
+        if (string.length() > NUMBER_LENGTH) {
+            string = string.substring(0, NUMBER_LENGTH);
+            string = string.concat("...");
+        }
+        return string;
     }
 
     //Метод, що малює число
-    private void drawTextInCenter(String text, Graphics graphics, int x, int y) {
+    private void drawTextInCenter(String key, String value, Graphics graphics, int x, int y) {
+        drawText(key, graphics, x, y);
+        drawText(value, graphics, x, y + height / 3);
+    }
+
+    private void drawText(String text, Graphics graphics, int x, int y) {
         TextLayout textLayout = new TextLayout(text, graphics.getFont(), graphics.getFontMetrics().getFontRenderContext());
         double textHeight = textLayout.getBounds().getHeight();
         double textWidth = textLayout.getBounds().getWidth();
         int textX = width / 2 - (int) textWidth / 2;
-        int textY = height / 2 + (int) textHeight / 2;
+        int textY = height / 3 + (int) textHeight / 2;
         graphics.drawString(text, textX + x, textY + y);
     }
-
 }
