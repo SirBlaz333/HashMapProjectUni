f016cf172eb221ed0678b8285d0b889f24a8120d
SirBlaz333 <89386578+SirBlaz333@users.noreply.github.com>
2022-12-22 13:55:52 +0200

    Update documentation

--- src/main/java/gui/hashmap/panel/image/text/TextDrawer.java
+++ src/main/java/gui/hashmap/panel/image/text/TextDrawer.java
@@ -12,7 +12,7 @@ public interface TextDrawer{
      * @param graphics графічний об'єкт
      * @param x координата х
      * @param y координата y
-     * @param key число
+     * @param entry входження мапи(ключ та значення)
      */
     void draw(Graphics graphics, int x, int y, Map.Entry<Integer, Integer> entry);
 }
